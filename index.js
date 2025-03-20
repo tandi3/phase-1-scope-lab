3408b0b4e2752f8f203c1299aba2626dd42a36c1
tandie oyucho <ysoyucho@gmail.com>
2025-03-20 11:28:07 +0300

    added changes

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+var BestCustomer;
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";    
+}
+const leastFavoriteCustomer = "John Doe";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Jane Doe";
+}
+
+
+
